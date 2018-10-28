9c5f45e7e059d43c7c9290b3f49e425188569257
Vu An <vuquocan@imac-cua-vu.local>
2018-10-28 12:41:35 +0700

    Initial Commit

---
+++ SetGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SetGame
+//
+//  Created by Vu Quoc An on 28/10/2018.
+//  Copyright © 2018 Vu Quoc An. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
